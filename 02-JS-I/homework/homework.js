ef433624b52d0e5d73ea2b4a7cf8dd49d2400f65
Cosm3No1de <fulanoide.cosme@gmail.com>
2022-10-07 18:06:09 -0300

    tercer commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola";
 const nuevoNum = 2;
 
 // Crea una variable booleana:
-const nuevoBool = trust;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -48,15 +48,15 @@ function resta(x, y) {
 }
 
 function multiplica(x, y) {
-  var mul = x * y;
-  return mul// Multiplica "x" por "y" y devuelve el valor
+
+  return x * y;// Multiplica "x" por "y" y devuelve el valor
   // Tu código:
   
 }
 
 function divide(x, y) {
-  var div = x / y; 
-  return div;// Divide "x" entre "y" y devuelve el valor
+  
+  return  x / y;// Divide "x" entre "y" y devuelve el valor
   // Tu código:
   
 }
@@ -75,13 +75,9 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:  
-  if (str1.length == str2.length)
- { return true;
- } else  { 
-  return false; 
-    }
-  }
-  
+  return str1.length == str2.length;
+ 
+}
 
 
 function menosQueNoventa(num) {
@@ -103,17 +99,15 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 50)
-  {return true;}
-  else {return false;}
+ return num >50; 
   
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  var resto = x % y;
-  return resto; 
+ 
+  return x % y; 
 }
 
 function esPar(num) {
@@ -152,8 +146,7 @@ function elevarAlCuadrado(num) {
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  var cub = num * num * num;
-   return cub;
+ return num * num * num;
   
 }
 
@@ -201,10 +194,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  var a = 'Hello world';
-  var b = '!';
-  a = a.concat(b);
-  console.log(a);
+  return str + '¡';
 
 }
 
@@ -212,19 +202,22 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var combi = nombre + ' ' +apellido;
+return combi
+
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return 'Hola' + nombre + '¡';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return alto * ancho;
   
 }
 
@@ -232,6 +225,8 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+
+  return lado * 4 
   
 }
 
@@ -239,6 +234,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  return  (base * altura)/2
 
 }
 
@@ -247,7 +243,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro * 1.2 
 }
 
 
@@ -257,8 +253,16 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  if (letra.lenght > 1){
+    return "Dato incorrecto"
+  }
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
+  }
   
-}
+
 
 
 
